01d04138885538047e2bb3c0f8d263254dd18606
yuichi-ff <yuichiro.f1002@gmail.com>
2024-10-20 23:04:26 +0900

    右上のボタンで終了できるように調整

--- emitter.py
+++ emitter.py
@@ -13,30 +13,33 @@ def reset_launchpad():
     for note in range(0, 128):
         mido.open_output(launchpad_out).send(Message('note_off', note=note, velocity=0))
         mido.open_output(loopmidi_out).send(Message('note_off', note=note, velocity=0))
+    for control_number in range(104, 112):
+        mido.open_output(launchpad_out).send(Message('control_change', control=control_number, value=0))
 
 atexit.register(reset_launchpad)
 
-color = 0
+COLOR = 62
 try:
     with    mido.open_input(launchpad_in) as inport, \
             mido.open_output(launchpad_out) as outport, \
             mido.open_output(loopmidi_out) as loopMIDI_outport:
+        
+        outport.send(Message('control_change', control=111, value=67))
         
         for msg in inport:
             print(msg)
             loopMIDI_outport.send(msg)
 
             if msg.type == 'note_on' and msg.velocity > 0:
-                color+=1
-                outport.send(Message('note_on', note=msg.note, velocity=color))
-                print("velocity",color)
+                outport.send(Message('note_on', note=msg.note, velocity=COLOR))
+                print("velocity",COLOR)
                 print(f"Button {msg.note} pressed. Velocity: {msg.velocity}")
             
             elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
                 outport.send(Message('note_off', note=msg.note, velocity=0))
                 print(f"Button {msg.note} released.")
 
-            if msg.type == 'note_on' and msg.note == 0:
+            if msg.type == 'control_change' and msg.control == 111:
                 break
 
 except KeyboardInterrupt:
